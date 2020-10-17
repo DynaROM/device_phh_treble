f98798e6f63fba13d6732b9c0947daa9c741a543
sooti <sooti85@gmail.com>
2020-10-17 11:04:40 +0300

    Add default aux whitelist for qcom devices

--- rw-system.sh
+++ rw-system.sh
@@ -62,6 +62,11 @@ fi
 setprop sys.usb.ffs.aio_compat true
 setprop persist.adb.nonblocking_ffs false
 
+# Enable Aux Camera for qcom (gcam)
+if getprop ro.boot.hardware|grep -iq  -e qcom;then
+   setprop vendor.camera.aux.packagelist "org.codeaurora.snapcam,com.android.camera,com.android.GoogleCamera"
+fi
+
 # Enable IMS for qcom devices
 if [ ! -f /mnt/phh/ims ];then
    mount -o remount,rw /
